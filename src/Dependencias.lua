8ccd62c944db6154a658458804c83747b67d31f8
Gaston Caminiti <gastoncaminiti@outlook.com>
2026-03-03 15:07:22 -0300

    Estructura de Proyecto
    Archivo Constantes
    Archivo Dependencias
    Clase MaquinaEstado
    Clase EstadoBase

---
+++ src/Dependencias.lua
@@ -0,0 +1,8 @@
+-- https://github.com/Ulydev/push
+Push = require 'lib.push'
+-- https://github.com/vrld/hump/blob/master/class.lua
+Class = require 'lib.class'
+
+require 'src.Constantes'
+require 'src.MaquinaEstado'
+require 'src.estados.EstadoBase'
---
+++ src/estados/EstadoBase.lua
@@ -0,0 +1,7 @@
+EstadoBase = Class{}
+
+function EstadoBase:init() end
+function EstadoBase:enter() end
+function EstadoBase:exit() end
+function EstadoBase:update(dt) end
+function EstadoBase:render() end
